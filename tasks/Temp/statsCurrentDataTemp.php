14ca22f3f55fad5dc9cd49629a789672a99ad56f
palinkas-jo-reggelt <52140617+palinkas-jo-reggelt@users.noreply.github.com>
2023-09-23 05:17:42 -0400

    tempdata

--- tasks/Temp/statsCurrentDataTemp.php
+++ tasks/Temp/statsCurrentDataTemp.php
@@ -1,8 +1,8 @@
 <?php
-$hitsC = 542;
+$hitsC = 562;
 $hitsR = 53;
 $hitsM = 4;
-$dataArrART = "1,542,3,3,34";
+$dataArrART = "1,562,3,3,34";
 $labelArrART = "'Client_Authenticated','Client_Connection','Incoming_Message','IP_RBL_Whitelisted','Record_PTR-HELO'";
 $countAcc_rows = 5;
 $dataArrRRT = "2,8,28,9,6";
